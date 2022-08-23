960e309bba908531cf57c61cbd8930557813ab3c
Mogakamo <mogaka.amo254@gmail.com>
2022-08-23 11:16:04 +0300

    schema: Category of dishes

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -2,9 +2,10 @@ import createSchema from "part:@sanity/base/schema-creator";
 
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import restaurant from "./restaurant";
+import category from "./category";
 
 export default createSchema({
   // We name our schema
   name: "default",
-  types: schemaTypes.concat([restaurant]),
+  types: schemaTypes.concat([restaurant, category]),
 });
